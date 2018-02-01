cc6d9d6680e080e63dc433bc124925d00bdefc48
Demian Holderegger <demian.holderegger@gmail.com>
2018-02-01 08:41:25 +0100

    add some measurements

--- electron-app/src/renderer/backend/Engine.js
+++ electron-app/src/renderer/backend/Engine.js
@@ -12,10 +12,14 @@ export class Engine {
         store.dispatch('setCSS', null);
         store.dispatch('setRenderTree', null);
         network.GET(url).then((data) => {
-            console.log(data);
+            let tStart = performance.now();
+            //console.log(data);
+            let tStartHtml = performance.now();
             let htmlParser = new HtmlParser();
             let nodes = htmlParser.parse(data);
             store.dispatch('setHTML', nodes);
+            console.log("Parsing HTML: " + Math.round(performance.now() - tStartHtml) + " milliseconds.");
+            let tStartCss = performance.now();
             let extractor = new HtmlStyleExtractor();
             let cssParser = new CssParser();
             let styles = extractor.extractStyles(nodes);
@@ -28,9 +32,13 @@ export class Engine {
                 }
             });
             store.dispatch('setCSS', styles);
+            console.log("Parsing CSS: " + Math.round(performance.now() - tStartCss) + " milliseconds.");
+            let tStartRendertree = performance.now();
             let renderTree = new RenderTree();
             let rtree = renderTree.createRenderTree(nodes, allStyleRules);
             store.dispatch('setRenderTree', rtree);
+            console.log("Create Rendertree: " + Math.round(performance.now() - tStartRendertree) + " milliseconds.");
+            console.log("Parsing & Rendering took " + Math.round(performance.now() - tStart) + " milliseconds.");
         });
     }
 }
--- electron-app/src/renderer/backend/Engine.ts
+++ electron-app/src/renderer/backend/Engine.ts
@@ -17,12 +17,17 @@ export class Engine {
         store.dispatch('setCSS', null);
         store.dispatch('setRenderTree', null);
         network.GET(url).then((data: string) => {
+            let tStart = performance.now();
 
-            console.log(data);
+            //console.log(data);
+
+            let tStartHtml = performance.now();
             let htmlParser = new HtmlParser();
             let nodes = htmlParser.parse(data);
             store.dispatch('setHTML', nodes);
+            console.log("Parsing HTML: " + Math.round(performance.now() - tStartHtml) + " milliseconds.");
 
+            let tStartCss = performance.now();
             let extractor = new HtmlStyleExtractor();
             let cssParser = new CssParser();
             let styles = extractor.extractStyles(nodes);
@@ -35,11 +40,15 @@ export class Engine {
                 }
             });
             store.dispatch('setCSS', styles);
+            console.log("Parsing CSS: " + Math.round(performance.now() - tStartCss) + " milliseconds.");
 
+            let tStartRendertree = performance.now();
             let renderTree = new RenderTree();
             let rtree = renderTree.createRenderTree(nodes, allStyleRules);
-
             store.dispatch('setRenderTree', rtree);
+            console.log("Create Rendertree: " + Math.round(performance.now() - tStartRendertree) + " milliseconds.");
+
+            console.log("Parsing & Rendering took " + Math.round(performance.now() - tStart) + " milliseconds.");
         });
     }
 }
