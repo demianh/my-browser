9000f12dcca8cd58b2a934464bab417f67655dbb
Demian Holderegger <demian.holderegger@gmail.com>
2020-01-26 14:00:17 +0100

    refactor test

--- tests/LayoutTree.test.js
+++ tests/LayoutTree.test.js
@@ -12,15 +12,17 @@ function createLayoutTree(html, css, width, height) {
 	let renderTree = new RenderTree();
 	let layoutTree = new LayoutTree();
 
+	let baseCss = 'div, p {display: block;}';
+
 	let nodes = htmlParser.parse(html);
-	let styles = cssParser.parse(css || '');
+	let styles = cssParser.parse(baseCss + (css || ''));
 	let tree = renderTree.createRenderTree(nodes, styles);
 	return layoutTree.createLayoutTree(tree, width || 200, height || 400);
 }
 
 test('Create Layout Tree', async t => {
 
-	let tree = createLayoutTree('<div>Hallo</div>', 'div {display: block;}', 200, 400);
+	let tree = createLayoutTree('<div>Hallo</div>', '', 200, 400);
 	t.is(200, tree[0].width);
 	t.is(16, tree[0].height);
 	t.is(0, tree[0].top);
@@ -32,25 +34,25 @@ test('display:block Width', async t => {
 
 	let tree;
 
-	tree = createLayoutTree('<div>Hallo</div>', 'div {display: block;}', 200, 400);
+	tree = createLayoutTree('<div>Hallo</div>', '', 200, 400);
 	t.is(200, tree[0].width);
 
-	tree = createLayoutTree('<div><p>Paragraph</p></div>', 'div {display: block; padding: 10px;} p {display: block;}', 200, 400);
+	tree = createLayoutTree('<div><p>Paragraph</p></div>', 'div {padding: 10px;}', 200, 400);
 	t.is(200, tree[0].width);
 	t.is(180, tree[0].children[0].width);
 
-	tree = createLayoutTree('<div><p>Paragraph</p></div>', 'div {display: block; padding: 10px; margin-left: 5px;} p {display: block;}', 200, 400);
+	tree = createLayoutTree('<div><p>Paragraph</p></div>', 'div {padding: 10px; margin-left: 5px;}', 200, 400);
 	t.is(200, tree[0].width);
 	t.is(175, tree[0].children[0].width);
 
-	tree = createLayoutTree('<div><p>Paragraph</p></div>', 'div {display: block; padding: 10px; margin-left: 5px; border: 1px solid black;} p {display: block;}', 200, 400);
+	tree = createLayoutTree('<div><p>Paragraph</p></div>', 'div {padding: 10px; margin-left: 5px; border: 1px solid black;}', 200, 400);
 	t.is(200, tree[0].width);
 	t.is(173, tree[0].children[0].width);
 
-	tree = createLayoutTree('<div style="width: 128px">Hallo</div>', 'div {display: block;}', 200, 400);
+	tree = createLayoutTree('<div style="width: 128px">Hallo</div>', '', 200, 400);
 	t.is(128, tree[0].width);
 
-	tree = createLayoutTree('<div style="width: 80%">Hallo</div>', 'div {display: block;}', 200, 400);
+	tree = createLayoutTree('<div style="width: 80%">Hallo</div>', '', 200, 400);
 	t.is(160, tree[0].width);
 
 });
@@ -63,7 +65,7 @@ test('display:inline Width', async t => {
 	t.is(40, tree[0].width);
 	t.is(40, tree[0].children[0].width);
 
-	tree = createLayoutTree('<div><span>Hallo</span><span>Welt</span></div>', '', 200, 400);
+	tree = createLayoutTree('<span><span>Hallo</span><span>Welt</span></span>', '', 200, 400);
 	t.is(72, tree[0].width);
 	t.is(40, tree[0].children[0].width);
 	t.is(32, tree[0].children[1].width);
@@ -83,7 +85,7 @@ test('display:inline Position', async t => {
 
 	let tree;
 
-	tree = createLayoutTree('<div><span>Hallo</span><span>Welt</span></div>', '', 200, 400);
+	tree = createLayoutTree('<span><span>Hallo</span><span>Welt</span></span>', '', 200, 400);
 	t.is(72, tree[0].width);
 	t.is(40, tree[0].children[0].width);
 	t.is(0, tree[0].children[0].top);
@@ -93,7 +95,7 @@ test('display:inline Position', async t => {
 	t.is(0, tree[0].children[1].top);
 	t.is(40, tree[0].children[1].left);
 
-	tree = createLayoutTree('<div>Hallo<span>Welt</span>!</div>', '', 200, 400);
+	tree = createLayoutTree('<span>Hallo<span>Welt</span>!</span>', '', 200, 400);
 	t.is(80, tree[0].width);
 	t.is(40, tree[0].children[0].width);
 	t.is(0, tree[0].children[0].top);
@@ -117,7 +119,7 @@ test('break text in block element', async t => {
 
 	let tree;
 
-	tree = createLayoutTree('<div>This text should wrap to multiple lines. We should get a few rows of text on multiple lines.</div>', 'div {display: block;}', 200, 400);
+	tree = createLayoutTree('<div>This text should wrap to multiple lines. We should get a few rows of text on multiple lines.</div>', '', 200, 400);
 	t.is(200, tree[0].width);
 	t.is(200, tree[0].children[0].width);
 	t.is(
@@ -130,7 +132,7 @@ test('break text in block element', async t => {
 		JSON.stringify(tree[0].children[0].textLines)
 	);
 
-	tree = createLayoutTree('<div>This text should wrap to multiple lines.</div>', 'div {display: block;}', 200, 400);
+	tree = createLayoutTree('<div>This text should wrap to multiple lines.</div>', '', 200, 400);
 	t.is(200, tree[0].width);
 	t.is(192, tree[0].children[0].width);
 	t.is(
